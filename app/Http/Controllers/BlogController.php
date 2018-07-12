8618431386f714e8358b09d6f6fbb52343708fb7
linxcodev <fajrul452@gmail.com>
2018-07-12 09:24:21 +0700

    CRUD Eloquent

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -10,6 +10,38 @@ class BlogController extends Controller
 {
     public function index()
     {
+      // insert cara biasa
+      // $data = new Blog;
+      // $data->title = 'halo kendal';
+      // $data->description = 'halo halo kendal halo halo kendal halo halo kendal';
+      // $data->save();
+
+      // insert by mass assigment
+      // Blog::create([
+      //   'id' => 100,
+      //   'title' => 'halo jg',
+      //   'description' => 'halo jg halo jg halo jg halo jg'
+      // ]);
+
+      // update cara 1
+      // $blog = Blog::find(6);
+      // $blog->id = 10;
+      // $blog->save();
+
+      // ,mass update
+      // Blog::where('id', 10)->update([
+      //   'id' => 6
+      // ]);
+
+      // delete biasa
+      // $blog = Blog::find(6);
+      // $blog->delete();
+
+      // delete menggunakan destroy (otomatis mencari id)
+      // Blog::destroy(5); single
+      // Blog::destroy([4,2]);
+
+      // menampilkan semua data
       $blog = Blog::all(); // select id all
 
       return view('blog/home', ['blogs' => $blog]);
@@ -17,8 +49,12 @@ class BlogController extends Controller
 
     public function show($id)
     {
+      // mnampilkan by id
       $blog = Blog::find($id); // mencari id
-      
+
+      if (!$blog)
+        dd("Id not found");
+
       return view('blog/blog', ['blogs' => $blog]); // buat variable blog di views
     }
 }
--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
+    // protected $table = 'my_blogs'; jika nama table berbeda
+    
     // jika di table kita tidak ada created_at and updated_at
-     public $timestamps = false;
+    public $timestamps = false;
+
+    // untuk methode mass assigment harus ada
+    // salah satu di antara dua ini
+    // protected $fillable = ['title', 'description']; // whitelist
+    protected $guarded = ['id']; // blocklist
 }
