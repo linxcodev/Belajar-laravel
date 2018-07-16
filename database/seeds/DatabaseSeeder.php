97589543b7884a4ed19b9bfb3c3ff08e5fa6c325
linxcodev <fajrul452@gmail.com>
2018-07-16 09:34:08 +0700

    Mengisi data automatis - menggunakan seed - php artisan make:seeder BlogsTableSeeder - faker data fake - php artisan db:seed --class=BlogsTableSeeer => spesifik - php artisan db:seed => banyak table - tapi masukkan class table di DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+      // jika table banyak
+      // gunakan php artisan db:seed
+        // $this->call(BlogsTableSeeder::class);
+        // $this->call(MisalTableSeeder::class);
+        // or
+        // $this->call([
+        //   UsersTableSeeder::class,
+        //   PostsTableSeeder::class,
+        //   CommentsTableSeeder::class,
+        // ]);
     }
 }
